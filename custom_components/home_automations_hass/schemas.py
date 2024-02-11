1b03be6a2a14aebb2a84c2e0021b491f6daf7f0c
Lennard Beers <l.beers@outlook.de>
2024-02-11 03:15:35 +0100

    fix: schema

--- custom_components/home_automations_hass/schemas.py
+++ custom_components/home_automations_hass/schemas.py
@@ -1,6 +1,5 @@
 import voluptuous as vol
 from homeassistant.const import CONF_URL
-from homeassistant.helpers import config_validation as cv
 
 from custom_components.home_automations_hass.const import (
     CONF_WASHING_MACHINE_MAC,
@@ -17,9 +16,9 @@ SCHEMA_CONFIG_FLOW_USER = vol.Schema(
 
 SCHEMA_OPTIONS_FLOW_USER = vol.Schema(
     {
-        vol.Required(CONF_WASHING_MACHINE_SHELLY_ENTITY_ID, default=""): cv.entity_id,
-        vol.Required(CONF_WASHING_MACHINE_MAC, default=""): cv.string,
-        vol.Required(CONF_WASHING_MACHINE_MANUFACTURER, default=""): cv.string,
-        vol.Required(CONF_WASHING_MACHINE_MODEL, default=""): cv.string,
+        vol.Required(CONF_WASHING_MACHINE_SHELLY_ENTITY_ID, default=""): str,
+        vol.Required(CONF_WASHING_MACHINE_MAC, default=""): str,
+        vol.Required(CONF_WASHING_MACHINE_MANUFACTURER, default=""): str,
+        vol.Required(CONF_WASHING_MACHINE_MODEL, default=""): str,
     }
 )
