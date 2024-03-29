277e0ae844d9269a025e91ab6dc513ce39d372cf
Lennard Beers <l.beers@outlook.de>
2024-03-29 22:04:50 +0100

    feat: timed light elevation control

--- home_automations/modules/timed_light_module.py
+++ home_automations/modules/timed_light_module.py
@@ -2,13 +2,14 @@ import logging
 
 from hass_client.models import Event, State
 
+from home_automations.helper.clock_events import ClockEvents
 from home_automations.models.config import Config
 from home_automations.models.timed_light_config import TimedLightConfig
 from home_automations.modules.base_module import BaseModule
 from home_automations.tools import Tools
 
 
-class TimedLightModule(BaseModule):
+class TimedLightModule(BaseModule, ClockEvents):
     timed_light_config: TimedLightConfig
 
     def __init__(
@@ -21,9 +22,17 @@ class TimedLightModule(BaseModule):
 
         self.timed_light_config = timed_light_config
 
-        self.register_state_changed(
-            self.on_schedule_changed, self.timed_light_config.schedule_entity
-        )
+        if self.timed_light_config.schedule_entity is not None:
+            self.register_state_changed(
+                self.on_schedule_changed, self.timed_light_config.schedule_entity
+            )
+        elif (
+            self.timed_light_config.on_elevation is not None
+            and self.timed_light_config.off_time is not None
+        ):
+            self.register_state_changed(
+                self.on_elevation_changed, "sensor.sonne_solar_elevation"
+            )
 
     async def on_off(self):
         if not await self.is_switch_on():
@@ -73,4 +82,30 @@ class TimedLightModule(BaseModule):
             await self.on_on()
 
         if new_state.state == "off":
+            await self.on_off()
+
+    async def on_elevation_changed(
+        self, event: Event, old_state: State, new_state: State
+    ) -> None:
+        if self.timed_light_config.on_elevation is None:
+            return
+
+        if (
+            float(old_state.state)
+            > self.timed_light_config.on_elevation
+            >= float(new_state.state)
+        ):
+            await self.on_on()
+
+    async def on_minute_changed(self, minute: int):
+        if self.timed_light_config.off_time is None:
+            return
+
+        current_time = self.tools.clock.current_time()
+        previous_time = current_time.replace(
+            minute=current_time.minute - 1 if current_time.minute > 0 else 59
+        )
+        off_time = self.tools.clock.parse_time(self.timed_light_config.off_time)
+
+        if previous_time < off_time <= current_time:
             await self.on_off()
