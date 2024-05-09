737b723cbf19ce494e52ec988ce4e386b18a47ae
Lennard Beers <l.beers@outlook.de>
2024-05-09 04:16:08 +0200

    fix: use default factory

--- home_automations/models/timed_light_config.py
+++ home_automations/models/timed_light_config.py
@@ -1,10 +1,10 @@
-from dataclasses import dataclass
+from dataclasses import dataclass, field
 
 
 @dataclass
 class TimedLightConfig:
     light_entities: list[str]
-    light_on_entities: list[str] = []
+    light_on_entities: list[str] = field(default_factory=list)
     schedule_entity: str | None = None
     switch_entity: str | None = None
     on_elevation: float | None = None
